04905e379f93f45da9793878afe9164c756d8d61
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-06-17 16:58:28 +0800

    try

--- pages/api/answer.ts
+++ pages/api/answer.ts
@@ -2,13 +2,19 @@ import { OpenAIStream } from "@/utils/answer";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 // import requestIp from "request-ip";
-
+export const localKey = import.meta.env.OPENAI_API_KEY || ""
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL || "",
   token: process.env.UPSTASH_REDIS_REST_TOKEN || "",
 })
 
-const ratelimit = new Ratelimit({
+const ratelimit_min = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.fixedWindow(1, "1 m"),
+  analytics: false,
+});
+
+const ratelimit_day = new Ratelimit({
   redis: redis,
   limiter: Ratelimit.fixedWindow(1, "1 m"),
   analytics: false,
@@ -20,14 +26,20 @@ export const config = {
 
 const handler = async (req: Request, res: Response): Promise<Response> => {
   const ipIdentifier = req.headers.get('x-real-cdn-ip') ?? req.headers.get('x-real-ip')
-  const result = await ratelimit.limit(`ai-search_${ipIdentifier}`);
+  const result_min = await ratelimit_min.limit(`ai-search_min${ipIdentifier}`);
+  const result_day = await ratelimit_day.limit(`ai-search_day${ipIdentifier}`);
   // res.headers.set('X-RateLimit-Limit', result.limit.toString())
   // res.headers.set('X-RateLimit-Remaining', result.remaining.toString())
   // res.headers.set('X-Reques-IP', ipIdentifier || '?')
 
-  if (!result.success) {
+  if (!result_min.success) {
     return new Response("Error: 搜索过快，请等待片刻。", { status: 200 });
   }
+
+  if (!result_day.success) {
+    return new Response("Error: 今天的搜索次数达到上限。", { status: 200 });
+  }
+
   try {
     const { prompt, apiKey } = (await req.json()) as {
       prompt: string;
