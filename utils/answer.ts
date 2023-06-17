c5820e1f99718362bc155a033a74f768535e3b42
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-06-17 18:11:58 +0800

    .

--- utils/answer.ts
+++ utils/answer.ts
@@ -15,7 +15,7 @@ export const OpenAIStream = async (prompt: string, apiKey: string) => {
       model: OpenAIModel.DAVINCI_TURBO,
       messages: [
         { role: "system", content: "You are a helpful assistant that accurately answers the user's queries based on the given text." },
-        { role: "user", content: "根据以下来源为查询提供1-2句话的答案。保持原创、简洁、准确和有帮助。在每个句子后引用来源 [1] 或 [2] 来支持你的答案（例如：正确：[1]，正确：[2]，不正确：[1,2])。所有回答都要使用中文，并且不超过400字: "+prompt }
+        { role: "user", content: prompt }
       ],
       max_tokens: 1000,
       temperature: 0.0,
