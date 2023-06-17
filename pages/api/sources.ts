cb92e86275670637155121693c5e120d596b6cf6
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-06-17 18:26:20 +0800

    Update sources.ts

--- pages/api/sources.ts
+++ pages/api/sources.ts
@@ -29,9 +29,9 @@ const ratelimit_filteredSources = [
   }
 ]
 
-const searchHandler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+const searchHandler = async (req: NextApiRequest, res: NextApiResponse<Data>, ratelimitreq: Request, ratelimitres: Response) => {
 
-  const ipIdentifier = req.headers.get('x-real-cdn-ip') ?? req.headers.get('x-real-ip')
+  const ipIdentifier = ratelimitreq.headers.get('x-real-cdn-ip') ?? ratelimitreq.headers.get('x-real-ip')
   const result = await ratelimit.limit(`ai-search_search${ipIdentifier}`);
 
   if (!result.success) {
